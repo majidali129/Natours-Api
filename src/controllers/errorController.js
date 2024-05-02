88769f7b6c2aa3eeffeaee84613e1da1aec7fa28
Majid Ali <lahmajidali@gmail.com>
2024-05-02 13:14:38 +0500

    implemented error handler for validations

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,9 +1,5 @@
 import { appError } from "../utils/appError.js";
 
-const handleCastErrorDB = (err) => {
-  const message = `Invalid ${err.path}: ${err.value}`;
-  return new appError(message, 400)
-}
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -18,21 +14,36 @@ const sendErrorProd = (err, res) => {
     res.status(err.statusCode).json({
     status: err.status,
     message: err.message
-    });
-    // programming error or other unknown:: don't leak error details;
-  }else{
-     // 1) Log error
-     console.error('ERROR 💥', err);
+  });
+  // programming error or other unknown:: don't leak error details;
+}else{
+  // 1) Log error
+  console.error('ERROR 💥', err);
 
-     // 2) Send generic message
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went wrong!!!'
-    });
-  }
-  }
+  // 2) Send generic message
+  res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong!!!'
+  });
+}
+}
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new appError(message, 400)
+}
 
+const handleDuplicateFieldsDB = (err) => {
+const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value`;
+  return new appError(message, 400)
+}
+
+const handleValidationError = err => {
+  const errors = Object.values(err.errors).map(el => el.message).join('. ');
+  const message = `Invalid input data. ${errors}`;
+  return new appError(message, 400)
+}
 const globalErrorController = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -42,7 +53,10 @@ const globalErrorController = (err, req, res, next) => {
     let error = {...err, message: err.message, name: err.name};
     // let error = JSON.parse(JSON.stringify(err));
     // error = {...error, message: err.message}
-    if (error.name === 'CastError') error = handleCastErrorDB(error)
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    // user origional err. b/c i need errmsg preperty which is not available on err
+    if (error.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (error.name === 'ValidationError') error = handleValidationError(err);
     sendErrorProd(error, res)
   }
 };
