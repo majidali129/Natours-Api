8647b95ae1086217d50de7b4d40a30bc6fa057fe
Majid Ali <lahmajidali@gmail.com>
2024-05-08 23:20:29 +0500

    refactoring code

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -1,10 +1,23 @@
 import { Review } from '../models/review.model.js';
 import { createOne, deleteOne, getAll, getOne, updateOne } from './handlerFactory.js';
 
+const addTourUserIds = (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
 const getAllReviews = getAll(Review);
 const addNewReview = createOne(Review);
 const deleteReview = deleteOne(Review);
 const updateReview = updateOne(Review);
 const getReview = getOne(Review);
 
-export { getAllReviews, addNewReview, deleteReview, updateReview, getReview };
+export {
+  getAllReviews,
+  addNewReview,
+  deleteReview,
+  updateReview,
+  getReview,
+  addTourUserIds
+};
